8afc3c6c8b367801d390b785d6054862953bf67a
AnnaCzinege <42236372+AnnaCzinege@users.noreply.github.com>
2020-03-30 14:11:22 +0200

    Create Movie model and context

---
+++ ImdbBackend/DataAccessLibrary/DataAccess/MovieContext.cs
@@ -0,0 +1,19 @@
+﻿using DataAccessLibrary.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLibrary.DataAccess
+{
+    class MovieContext : DbContext
+    {
+        public MovieContext(DbContextOptions options) : base(options)
+        { }
+            public DbSet<Movie> Movies { get; set; }
+            public DbSet<Genre> Genres { get; set; }
+            public DbSet<Language> Languages { get; set; }
+        
+    }
+
+}
---
+++ ImdbBackend/DataAccessLibrary/Models/Genre.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataAccessLibrary.Models
+{
+    public class Genre
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Name { get; set; }
+    }
+}
