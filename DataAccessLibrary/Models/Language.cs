bca72975f856d7064832ccff7845454c12fe07b2
esztermazi <49866977+esztermazi@users.noreply.github.com>
2020-03-30 15:13:01 +0200

    Add Models and DataAccessLibrary project to backend solution

---
+++ DataAccessLibrary/Models/Language.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace DataAccessLibrary.Models
+{
+    public class Language
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(200)]
+        public string Name { get; set; }
+    }
+}
