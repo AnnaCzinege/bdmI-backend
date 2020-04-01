748b3c6b5c8ce1480e10079193a62e41376c3750
AnnaCzinege <42236372+AnnaCzinege@users.noreply.github.com>
2020-04-01 20:27:39 +0200

    Create AllMoviesController.cs

---
+++ ImdbBackend/Controllers/AllMoviesController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataAccessLibrary.DataAccess;
+using DataAccessLibrary.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace BackupProject.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AllMoviesController : ControllerBase
+    {
+        private readonly ILogger<AllMoviesController> _logger;
+        private readonly MovieContext _db;
+
+        public AllMoviesController(ILogger<AllMoviesController> logger, MovieContext db)
+        {
+            _logger = logger;
+            _db = db;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Movie>>> GetAllMovies()
+        {
+            var movies = _db.Movies;
+            return await movies.ToListAsync();
+        }
+    }
+}
