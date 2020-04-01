98ddbcc03b6058f251f8b51d5fa9df2cb120df24
esztermazi <49866977+esztermazi@users.noreply.github.com>
2020-04-01 16:00:31 +0200

    Implement get popular movies per page

--- ImdbBackend/Controllers/PopularController.cs
+++ ImdbBackend/Controllers/PopularController.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DataAccessLibrary.DataAccess;
+using DataAccessLibrary.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace ImdbBackend.Controllers
@@ -13,16 +16,20 @@ namespace ImdbBackend.Controllers
     public class PopularController : ControllerBase
     {
         private readonly ILogger<PopularController> _logger;
+        private readonly MovieContext _db;
 
-        public PopularController(ILogger<PopularController> logger)
+        public PopularController(ILogger<PopularController> logger, MovieContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
-        [HttpGet]
-        public string Get()
+        [HttpGet("{page}")]
+        public async Task<ActionResult<IEnumerable<Movie>>> GetPopularMovies(int page)
         {
-            return "Cinci egy szar!";
+            int moviesPerPage = 20;
+            int from = page * moviesPerPage - 19;
+            return await _db.Movies.OrderByDescending(movie => movie.Popularity).Skip(from).Take(moviesPerPage).Select(movie => movie).ToListAsync();
         }
     }
 }
