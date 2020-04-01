36ab2fc14e8010840bd92dec500c7b47c823883d
Rasgacode <rongybaba12@gmail.com>
2020-04-01 15:26:55 +0200

    Implement PopularController
    Co-Authored-By: esztermazi <esztermazi@users.noreply.github.com>
    Co-Authored-By: annaczinege <annaczinege@users.noreply.github.com>

---
+++ ImdbBackend/Controllers/PopularController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ImdbBackend.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PopularController : ControllerBase
+    {
+        private readonly ILogger<PopularController> _logger;
+
+        public PopularController(ILogger<PopularController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Cinci egy szar!";
+        }
+    }
+}
