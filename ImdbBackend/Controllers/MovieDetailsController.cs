67183ea85c00adeeb7654480a1c24e54e01cba45
AnnaCzinege <42236372+AnnaCzinege@users.noreply.github.com>
2020-04-02 13:50:14 +0200

    Create model for MovieDetails to send to frontend

--- ImdbBackend/Controllers/MovieDetailsController.cs
+++ ImdbBackend/Controllers/MovieDetailsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Web.Http.Cors;
 using DataAccessLibrary.DataAccess;
+using DataAccessLibrary.FetchModels;
 using DataAccessLibrary.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -26,10 +27,34 @@ namespace BackupProject.Controllers
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<Movie>> GetMovieDetails(int id)
+        public async Task<ActionResult<FetchModelForMovieDetails>> GetMovieDetails(int id)
         {
             var movie = await _db.Movies.FindAsync(id);
-            return movie;
+            var movieGenreIds = _db.MovieGenres.Where(mg => mg.MovieId == id).Select(mg => mg.GenreId);
+            var movieLanguageIds = _db.MovieLanguages.Where(ml => ml.MovieId == id).Select(ml => ml.LanguageId);
+            List<Genre> genres = _db.Genres.Where(g => movieGenreIds.Contains(g.Id)).ToList();
+            List<Language> languages = _db.Languages.Where(l => movieLanguageIds.Contains(l.Id)).ToList();
+            FetchModelForMovieDetails MovieDetails = ConvertMovieObject(movie, genres, languages);
+            return MovieDetails;
+        }
+
+        private FetchModelForMovieDetails ConvertMovieObject(Movie movie, List<Genre> genres, List<Language> languages)
+        {
+            FetchModelForMovieDetails movieDetails = new FetchModelForMovieDetails()
+            {
+                Id = movie.Id,
+                OriginalId = movie.OriginalId,
+                OriginalTitle = movie.OriginalTitle,
+                Overview = movie.Overview,
+                MovieGenres = genres,
+                MovieLanguages = languages,
+                ReleaseDate = movie.ReleaseDate,
+                Runtime = movie.Runtime,
+                VoteCount = movie.VoteCount,
+                VoteAverage = movie.VoteAverage,
+                PosterPath = movie.PosterPath
+            };
+            return movieDetails;
         }
     }
 }
