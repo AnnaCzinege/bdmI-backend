c430a5fdad9b00e9cacc24afec082d04bbec4cca
AnnaCzinege <42236372+AnnaCzinege@users.noreply.github.com>
2020-04-02 09:41:34 +0200

    Configure CORS policy in Startup.cs

--- ImdbBackend/Startup.cs
+++ ImdbBackend/Startup.cs
@@ -23,11 +23,21 @@ namespace ImdbBackend
             Configuration = configuration;
         }
 
+        readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy(MyAllowSpecificOrigins,
+                builder =>
+                {
+                    builder.WithOrigins("http://localhost").AllowAnyHeader().AllowAnyMethod();
+                });
+            });
             services.AddDbContext<MovieContext>(option => { option.UseSqlServer(Configuration.GetConnectionString("Default")); });
             services.AddHostedService<UpdateDatabase>();
             services.AddControllers();
@@ -41,9 +51,13 @@ namespace ImdbBackend
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseCors(MyAllowSpecificOrigins);
+
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors();
 
             app.UseAuthorization();
 
