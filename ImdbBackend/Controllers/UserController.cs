41858185378cb536be9d84a2b9ecff1b9ae25ff0
AnnaCzinege <42236372+AnnaCzinege@users.noreply.github.com>
2020-04-14 14:41:07 +0200

    Create UserController.cs

---
+++ ImdbBackend/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataAccessLibrary.RepositoryContainer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ImdbBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UserController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+    }
+}
