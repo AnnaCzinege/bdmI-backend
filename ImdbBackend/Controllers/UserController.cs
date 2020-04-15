581dc5dfa84230dda06ea075a8ecd8df02aadb0f
AnnaCzinege <42236372+AnnaCzinege@users.noreply.github.com>
2020-04-15 10:17:54 +0200

    Update UserController

--- ImdbBackend/Controllers/UserController.cs
+++ ImdbBackend/Controllers/UserController.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DataAccessLibrary.Models;
 using DataAccessLibrary.RepositoryContainer;
+using ImdbBackend.ApiModels;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ImdbBackend.Controllers
@@ -13,10 +16,25 @@ namespace ImdbBackend.Controllers
     public class UserController : ControllerBase
     {
         private readonly IUnitOfWork _unitOfWork;
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
 
-        public UserController(IUnitOfWork unitOfWork)
+        public UserController(IUnitOfWork unitOfWork,
+            UserManager<User> userManager,
+            SignInManager<User> signInManager)
         {
             _unitOfWork = unitOfWork;
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<UserRegister>> Register([FromBody]UserRegister model)  // UserDTO!
+        {
+            Console.WriteLine(model.Email);
+            Console.WriteLine(model.UserName);
+            return model;
+            
         }
 
     }
