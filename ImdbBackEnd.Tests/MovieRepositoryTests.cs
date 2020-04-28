e2e7c06afec5de50c40bf2956d754abff178951f
AnnaCzinege <42236372+AnnaCzinege@users.noreply.github.com>
2020-04-28 18:57:19 +0200

    Add Test_GetMovieById test method
    Co-Authored-By: rasgacode <rasgacode@users.noreply.github.com>
    Co-Authored-By: esztermazi <esztermazi@users.noreply.github.com>

--- ImdbBackEnd.Tests/MovieRepositoryTests.cs
+++ ImdbBackEnd.Tests/MovieRepositoryTests.cs
@@ -1,9 +1,13 @@
+using Autofac;
 using DataAccessLibrary.DataAccess;
+using DataAccessLibrary.Models;
 using DataAccessLibrary.RepositoryContainer;
+using ImdbBackend;
 using Microsoft.EntityFrameworkCore;
 using NSubstitute;
 using NUnit.Framework;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace ImdbBackEnd.Tests
 {
@@ -12,6 +16,7 @@ namespace ImdbBackEnd.Tests
         private DbContextOptions _options;
         private MovieContext _context;
         private IUnitOfWork _unitOfWork;
+        private IContainer _container;
 
         [SetUp]
         public void Setup()
@@ -20,13 +25,58 @@ namespace ImdbBackEnd.Tests
                 .UseInMemoryDatabase(databaseName: "DbContextInMemory")
                 .Options;
             _context = new MovieContext(_options);
+            _context.Movies.Add(new Movie()
+            {
+                Id = 1,
+                OriginalId = 1,
+                OriginalTitle = "Title 1",
+                Overview = "First movie in fake db.",
+                ReleaseDate = "2020-04-28",
+                Runtime = 120,
+                VoteAverage = 8.7,
+                VoteCount = 2564,
+                Popularity = 25.4,
+                PosterPath = "nope"
+            });
+            _context.Genres.Add(new Genre()
+            {
+                Id = 1,
+                Name = "Drama"
+            });
+            _context.Languages.Add(new Language()
+            {
+                Id = 1,
+                Name = "English"
+            });
+            _context.MovieGenres.Add(new MovieGenre()
+            {
+                GenreId = 1,
+                MovieId = 1
+            });
+            _context.MovieLanguages.Add(new MovieLanguage()
+            {
+                LanguageId = 1,
+                MovieId = 1
+            });
             _unitOfWork = Substitute.For<IUnitOfWork>();
+            _container = GetMockedContainer(_context, _unitOfWork);
         }
 
         [Test]
-        public void Test1()
+        public async Task Test_GetMovieByOriginalId()
         {
-            Assert.Pass();
+            Movie movie = await _unitOfWork.MovieRepository.GetMovieByOriginalId(420818);
+            Assert.AreEqual("The Lion King", movie.OriginalTitle);
+        }
+
+        private IContainer GetMockedContainer(MovieContext context, IUnitOfWork unitOfWork)
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterInstance(context).As<MovieContext>();
+            builder.RegisterInstance(unitOfWork).As<IUnitOfWork>();
+
+            return builder.Build();
         }
     }
 }
